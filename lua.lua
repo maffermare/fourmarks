422f0c4fa79339cfac1dfd387378edaf801d6dca
maffermare <153961508+maffermare@users.noreply.github.com>
2025-01-14 09:04:28 -0600

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,15 +1,10 @@
-/four-marks-adventure
+/project-root
 |-- index.html
 |-- _redirects
-|-- /quests
-    |-- shrine.html
-    |-- service.html
-    |-- morality.html
-    |-- prayer.html
 |-- /css
-    |-- style.css
-|-- /js
-    |-- shrine.js
-    |-- service.js
-    |-- morality.js
-    |-- prayer.js
+|   |-- style.css
+|-- /quests
+|   |-- shrine.html
+|   |-- service.html
+|   |-- morality.html
+|   |-- prayer.html
